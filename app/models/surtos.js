4332829cbee9972f9fefd9e1e419667e640a3ede
riiiasg <ritagomes94@sapo.pt>
2023-11-14 16:27:07 +0000

    fiz recomendacoes.js

--- app/models/surtos.js
+++ app/models/surtos.js
@@ -5,7 +5,7 @@ var SurtosSchema   = new Schema({
     cod_surto: {type: Number, nullable:false}, //será assim??
     cod_zonageo: {type: Number, nullable:false},
     data_inicio: {type: Date, nullable:false},
-    data_fim: {type: Date, nullable:false}
+    data_fim: {type: Date, nullable:false} 
 });
 
 module.exports = mongoose.model('surtos', SurtosSchema);
