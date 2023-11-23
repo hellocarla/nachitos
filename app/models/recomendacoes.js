6d8493fc5d1e3bcf784d5f0153827054b53584fa
riiiasg <ritagomes94@sapo.pt>
2023-11-23 22:42:50 +0000

    Coloquei restrições em todos os códigos e apaguei os console.log perdidos

--- app/models/recomendacoes.js
+++ app/models/recomendacoes.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+function validarNumeroPositivo(value) {
+        return value>0;
+}
+
 var RecomendacoesSchema = new Schema({
     //cod_pais: {type: Schema.Types.ObjectId, ref: 'paises'},     //FK para ligar as recomendações aos países
     cod_recomendacao: {type: String, required:true, unique: true}, 
     cod_surto: {type: Schema.Types.ObjectId, ref: 'surtos'},    //FK para ligar as recomendações às surtos
     cod_zonageo: {type: Schema.Types.ObjectId, ref: 'zona'},    //FK para ligar as recomendações às zonas
     data_nota: {type: Date, required: true},    
-    validade_nota: {type: Number, default:null}, 
+    validade_nota: {type: Number, default:null, validate: [validarNumeroPositivo, 'A validade da nota deve ser maior ou igual a 1 dia']}, 
     //validade_nota: {type: Date, required: true},      
-    recomendacao_texto: {type: String, default: null}  
+    recomendacao_texto: {type: String, required:true}  
 });
 module.exports = mongoose.model('recomendacoes', RecomendacoesSchema);
--- app/models/surtos.js
+++ app/models/surtos.js
@@ -6,8 +6,11 @@ var SurtosSchema   = new Schema({
     cod_surto: {type: String, required:true, unique:true}, 
     cod_zonageo: {type: Schema.Types.ObjectId, ref: 'zona'},    // FK que liga os surtos à zona 
     data_inicio: {type: Date, required:true},  
-    data_fim: {type: Date, default:null},
-    cod_virus: {type: Schema.Types.ObjectId, ref: 'virus'}
+    data_fim: {type: Date, validate: {
+            validator: function(value) {
+                return !this.data_inicio || !value || this.data_inicio <= value;
+            }, message: 'A data de fim deve ser posterior a data de início.'}, default: null},
+    cod_virus: {type: Schema.Types.ObjectId, ref: 'virus' }
 });
 
 module.exports = mongoose.model('surtos', SurtosSchema);
