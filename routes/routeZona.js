711fe077707ef73e9f710d63c91cb94443b71fd9
riiiasg <ritagomes94@sapo.pt>
2023-11-27 16:40:43 +0000

    coloquei verificações e acrescentei os get pelos códigos

--- routes/routeZona.js
+++ routes/routeZona.js
@@ -2,11 +2,18 @@ var express = require('express');
 var Zona = require('../app/models/zona');
 var router = express.Router();
 
-// on routes that end with /zona
-// ou seja, todos os caminhos que passarem pelo model/zona
 
 // POST http://localhost:8082/api/zona
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
+    try{
+        const check_zona = await Zona.findOne({cod_zonageo: req.body.cod_zonageo});
+        if (check_zona) {
+            return res.status(409).json({ message: 'A zona com o código ' + req.body.cod_zonageo + ' já existe!' });
+        }
+        const check_zona_nome = await Zona.findOne({nome_zonageo: req.body.nome_zonageo});
+        if (check_zona_nome) {
+            return res.status(409).json({ message: 'A zona com o nome ' + req.body.nome_zonageo + ' já existe.' });
+        }
     var zona = new Zona();                            // criar instância de modelo de zona
     zona.cod_zonageo = req.body.cod_zonageo;             // aqui vão os valores que queremos
     zona.nome_zonageo = req.body.nome_zonageo;
@@ -15,17 +22,17 @@ router.post('/', function (req, res) {
             res.send(err);
         } else {
              res.json({ message: 'new area unlocked!' });       // mensagem de sucesso
-        }
-            
-
-       
+        } 
     });
-
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro no TRY!' });
+    }
 });
 
 
-// GET das zonas
-
+// GET de todas as zonas
 router.get('/', function(_req,res) {
     Zona.find(function(err, zona) {
         if(err)
@@ -33,6 +40,43 @@ router.get('/', function(_req,res) {
 
         res.json(zona);
     });
+});
+
+
+// Get de uma zona específica
+router.get('/:cod_zonageo', async function(req,res) {
+    try{
+        const zona = await Zona.findOne({cod_zonageo: req.params.cod_zonageo});
+        if (!zona) {
+            return res.status(404).json({ message: 'A zona com o código ' + req.params.cod_zonageo + ' não existe!' });
+        }
+        res.json(zona);
+    }  
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erro no TRY!' });
+    }
+});
+
+
+// Delete de uma zona geográfica pelo código de zona
+router.delete('/:cod_zonageo', async function (req, res) {
+    try {
+        var zona = await Zona.findOne({cod_zonageo:req.params.cod_zonageo}).exec();
+            // Se a zona não existir, envia uma resposta a indicar que não foi encontrado
+        if (!zona) {
+            return res.status(404).json({ message: 'Zona com o código ' + req.params.cod_zonageo + ' não encontrado!' });
+        }
+        const resultadoExclusao = await Zona.findByIdAndDelete(zona._id);
+        if (!resultadoExclusao) {
+            return res.json({ message: 'Zona não encontrada ou já excluído!' });
+        }
+        res.json({ message: 'Zona eliminada!' });
+
+    } catch (error) {
+        console.error(error);
+        res.json({ error: 'Erro no TRY!' });
+    }
 });
 
 
