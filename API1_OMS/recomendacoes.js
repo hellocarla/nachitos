104c95bdc9a67cbfe995d555532709eb08b3e177
inesfernandes <inesdejesusfernandes0@gmail.com>
2023-12-27 12:37:01 +0000

    adicionei modelos de recomendações e surtos à API

---
+++ API1_OMS/recomendacoes.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+function validateCodRecomendacao(value) {
+    return /^R\d+/.test(value);        // Usa uma regex para verificar se o valor começa com R maiusculo.
+}
+function validarNumeroPositivo(value) {
+        return value>0;
+}
+
+var RecomendacoesSchema = new Schema({
+    cod_recomendacao: {type: String, required:true, unique: true, validate: [validateCodRecomendacao, 'O campo cod_recomendacao deve sempre seguir a estrutura de letra R maiúscula seguida de digitos. Exemplo: R001']}, 
+    cod_surto: {type: Schema.Types.ObjectId, ref: 'surtos'},    //FK para ligar as recomendações às surtos
+    cod_zonageo: {type: Schema.Types.ObjectId, ref: 'zona'},    //FK para ligar as recomendações às zonas
+    data_nota: {type: Date, required: true},    
+    validade_nota: {type: Number, default:null, validate: [validarNumeroPositivo, 'A validade da nota deve ser maior ou igual a 1 dia']},      
+    recomendacao_texto: {type: String, required:true}  
+});
+module.exports = mongoose.model('recomendacoes', RecomendacoesSchema);
