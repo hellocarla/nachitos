5cf9ca59bab44105ad68cf7d947dd97f7e60640f
CV <carlavieira1990rgmail.com>
2023-12-28 12:30:46 +0000

    28122023 -- created & added destination & package routes

---
+++ SafetyTourism/models/destinations.js
@@ -0,0 +1,12 @@
+// NOT MONGOOSED YET -- ON PURPOSE
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const DestinationsSchema = new Schema({
+    id: Schema.Types.ObjectId,
+    city_name: String,      // name of city
+    city_desc: String,      // brief description of city
+    country_name: String    // country it belongs to (make it grab from database)
+});
+
+module.exports = mongoose.model('destinations', DestinationsSchema);
---
+++ SafetyTourism/models/packages.js
@@ -0,0 +1,13 @@
+// NOT MONGOOSED YET -- ON PURPOSE
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const PackagesSchema = new mongoose.Schema({        // <-- why is it like this in the teacher's example?
+    id: Schema.Types.ObjectId,
+    city: {type: Schema.Types.ObjectId, ref: 'destinations'},    
+    pack_desc: String,      // brief description of package
+    pack_price: String,     // price of pack(age) (does it change from type to type?)
+    pack_type: String       // hotel, plane, hotel+plane
+});
+
+module.exports = mongoose.model('packages', PackagesSchema);
