01639fcbe7b7468265c34d0e88479ed7f57a3d85
riiiasg <ritagomes94@sapo.pt>
2023-12-30 17:44:51 +0000

    Adicionei funcionalidades necessárioas para trabalhar com o frontend

--- SafetyTourism/routes/packagesRoutes.js
+++ SafetyTourism/routes/packagesRoutes.js
@@ -2,7 +2,7 @@
 
 // IMPORT requirements
 const express = require('express');
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, Segments } = require('celebrate');
 // IMPORT the controller
 const packagesController = require('../controllers/packagesController');
 var router = express.Router();
@@ -10,7 +10,7 @@ var router = express.Router();
 // POST WITH JOI (YAY)
 router.post('/', celebrate({
     body: Joi.object({
-        cityId: Joi.string().required(),
+        city: Joi.string().required(),
         pack_desc: Joi.string().min(5).max(280).regex(/^[a-zA-ZÀ-ÖØ-öø-ÿÇç\s]+$/),
         pack_price: Joi.string().required(),
         pack_type: Joi.string().valid('Hotel', 'Avião', 'Hotel e Avião').required()
@@ -25,19 +25,42 @@ router.get('/:id', packagesController.getPackagesById);
 
 router.get('/city/:city', packagesController.getPackagesByName);
 
-//router.put('/:id', packagesController.updatePackages);
 router.put(
-    '/:id',
-    celebrate({
-      body: Joi.object({
-        city: Joi.string(),
-        pack_desc: Joi.string().min(5).max(280),
-        pack_price: Joi.string(),
-        pack_type: Joi.string().valid('Hotel', 'Avião', 'Hotel e Avião')
-      })
+  '/:id',
+  (req, res, next) => {
+    next();
+  },
+  
+  celebrate({
+    [Segments.PARAMS]: Joi.object({
+      id: Joi.string().required()
     }),
-    packagesController.updatePackages
-  );
+    [Segments.BODY]: Joi.object({
+      _id: Joi.string().required(),
+      city: Joi.string(),
+      pack_desc: Joi.string().min(5).max(280),
+      pack_price: Joi.string(),
+      pack_type: Joi.string().valid('Hotel', 'Avião', 'Hotel e Avião')
+    })
+  }),
+  packagesController.updatePackages
+);
+
+/*
+router.put(
+  '/:id',
+  celebrate({
+    body: Joi.object({
+      id: Joi.string(),
+      city: Joi.string(),
+      pack_desc: Joi.string().min(5).max(280),
+      pack_price: Joi.string(),
+      pack_type: Joi.string().valid('Hotel', 'Avião', 'Hotel e Avião')
+    })
+  }),
+  packagesController.updatePackages
+);
+*/
 
 router.delete('/:id', packagesController.deletePackages);
 
